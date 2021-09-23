8f4d473e5e11615a762df350d9a059323f51e5f3
victra <victra.sonata@ottodigital.id>
2021-09-23 12:23:08 +0700

    add make deploy

--- github/auto-deploy.php
+++ github/auto-deploy.php
@@ -1,11 +1,10 @@
 <?php
 
 // Use in the “Post-Receive URLs” section of your GitHub repo.
-
-if ( $_POST['payload'] ) {
-shell_exec('cd /Users/victra/Projek/office/ottodigital/scripts');
-shell_exec('make run_local');
-echo "success deploy";
+if ( isset($_POST) ) {
+    $logs = shell_exec('make run_deploy');
+    echo "$logs \n";
+    echo "success deploy";
 }
 
 ?>
