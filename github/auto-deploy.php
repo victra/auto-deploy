120e1f683d06a3388f2dafd0f7e22e72d7c1871b
victra <victra.sonata@ottodigital.id>
2021-09-24 09:42:17 +0700

    add shell script edit

--- github/auto-deploy.php
+++ github/auto-deploy.php
@@ -2,7 +2,7 @@
 
 // Use in the “Post-Receive URLs” section of your GitHub repo.
 if ( isset($_POST) ) {
-    $logs = shell_exec('run-script.sh');
+    $logs = shell_exec('./run-script.sh');
     echo "$logs \n";
     echo "success deploy";
 }
